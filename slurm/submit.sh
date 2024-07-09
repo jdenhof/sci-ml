ef64c4fff1e1c51e322e5a65d93c9913f145d116
Jagger Denhof <denhofja@mail.gvsu.edu>
2024-07-09 11:40:24 -0400

    changed submit partition to all

--- slurm/submit.sh
+++ slurm/submit.sh
@@ -4,6 +4,7 @@
 #SBATCH --error=.snakemake/slurm_logs/submission/job.%j.err
 #SBATCH --time=24:00:00
 #SBATCH --cpus-per-task=1
+#SBATCH --partition=all
 #SBATCH --mem=4G
 
 snakemake --profile slurm "$@"
